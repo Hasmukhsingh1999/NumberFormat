ddca0e6a19fece1ed30ec6517ea41fc56c9c0c44
Hasmukh <hasmukhsingh90@gmail.com>
2023-12-02 10:50:25 +0530

    fixed

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
+"use client"
 import React from "react";
 import Output from "./components/Output";
 
 const page = () => {
+  const handleInputChangeActualCost = (e) => {};
+  const handleInputChangeSupplierCost = (e) => {};
 
-
-  const handleInputChangeActualCost = (e)=>{}
-  const handleInputChangeSupplierCost = (e)=>{}
-  
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center gap-9">
       <h1 className="text-4xl font-bold text-gray-900">FORM HANDLER</h1>
@@ -16,16 +15,14 @@ const page = () => {
           type="text"
           placeholder="Actual Cost"
           name="actual_cost"
- 
           onChange={handleInputChangeActualCost}
- />
+        />
         <input
           className="border border-gray-400  p-4 rounded-xl"
           type="text"
           placeholder="Supplier Cost"
           name="supplier_cost"
           onChange={handleInputChangeSupplierCost}
-
         />
         <button
           type="submit"
