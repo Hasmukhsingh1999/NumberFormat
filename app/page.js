fa880403f6ad1079df371af9864d755b11c164f6
Hasmukh <hasmukhsingh90@gmail.com>
2023-12-02 10:47:24 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React from "react";
+import Output from "./components/Output";
 
 const page = () => {
-  return (
-    <div className='h-screen w-full flex flex-col items-center justify-center gap-9'>
-      <h1 className='text-4xl font-bold text-gray-900'>FORM HANDLER</h1>
-      <form className='flex gap-5'>
-        <input className='border border-gray-400 p-4 rounded-xl' type='text' placeholder='Actual Cost' name='actual_cost'/>
-        <input className='border border-gray-400  p-4 rounded-xl' type='text' placeholder='Supplier Cost' name='supplier_cost'/>
-        <button type='submit' className='p-4 bg-orange-600 rounded-xl text-white font-semibold'>Submit</button>
-      </form>
-    </div>
-  )
-}
 
-export default page
+
+  const handleInputChangeActualCost = (e)=>{}
+  const handleInputChangeSupplierCost = (e)=>{}
+  
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-9">
+      <h1 className="text-4xl font-bold text-gray-900">FORM HANDLER</h1>
+      <form className="flex gap-5">
+        <input
+          className="border border-gray-400 p-4 rounded-xl"
+          type="text"
+          placeholder="Actual Cost"
+          name="actual_cost"
+ 
+          onChange={handleInputChangeActualCost}
+ />
+        <input
+          className="border border-gray-400  p-4 rounded-xl"
+          type="text"
+          placeholder="Supplier Cost"
+          name="supplier_cost"
+          onChange={handleInputChangeSupplierCost}
+
+        />
+        <button
+          type="submit"
+          className="p-4 bg-orange-600 rounded-xl text-white font-semibold"
+        >
+          Submit
+        </button>
+      </form>
+      {/* OUTPUT  */}
+      <div>
+        <Output />
+      </div>
+      {/* OUTPUT  */}
+    </div>
+  );
+};
+
+export default page;
