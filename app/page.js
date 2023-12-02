ecbdcd9c60fa77bcf955b3e5c78d9c8e518d6672
Hasmukh <hasmukhsingh90@gmail.com>
2023-12-02 10:33:46 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='h-screen w-full'>
+      <form>
+        
+      </form>
+    </div>
   )
 }
 
