08437f40263082a3a8150efc53882d76d2bbd32a
Hasmukh <hasmukhsingh90@gmail.com>
2023-12-02 10:26:46 +0530

    my first commit

--- app/page.js
+++ app/page.js
@@ -1,35 +1,9 @@
-"use client";
-import React, { useState } from "react";
+import React from 'react'
 
-const NumberFormatter = () => {
-  const [inputNumber, setInputNumber] = useState("");
-
-  // Function to format the number as "9,999.99"
-  const formatNumber = (num) => {
-    return num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
-  };
-
-  const handleInputChange = (event) => {
-    const inputValue = event.target.value.replace(/[^0-9.]/g, "");
-    setInputNumber(inputValue);
-  };
-
+const page = () => {
   return (
-    <div>
-      <label>
-        Enter Number:
-        <input
-          className="text-black"
-          type="text"
-          value={inputNumber}
-          onChange={handleInputChange}
-        />
-      </label>
-      {inputNumber && (
-        <p>Formatted Number: {formatNumber(parseFloat(inputNumber))}</p>
-      )}
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default NumberFormatter;
+export default page
